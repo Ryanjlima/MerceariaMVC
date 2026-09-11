fc5d97730e4c65efac4b8fb721a6e840f0859cdc
Ryanjlima <rjlimabl06@gmail.com>
2026-09-11 15:37:51 -0300

    Adicionar arquivos de projeto.

---
+++ MerceariaMVC/Data/MerceariaMVCContext.cs
@@ -0,0 +1,17 @@
+﻿using MerceariaMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MerceariaMVC.Data
+{
+    public class MerceariaMVCContext:DbContext
+    {
+        public MerceariaMVCContext(DbContextOptions<MerceariaMVCContext>options):base(options) 
+        {
+            
+        }
+        public DbSet<Produto> Produtos {  get; set; }
+
+        public DbSet<Cliente> clientes { get; set; }
+      
+    }
+}
---
+++ MerceariaMVCTests/ProdutoTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MerceariaMVC.Models;
+
+namespace MerceariaMVCTests
+{
+    public class ProdutoTests
+    {
+        [Fact]
+        public void Verificar_Preco_Invalido_se_ForMenorqueZero()
+        {
+            var produto = new Produto
+            {
+                Nome = "Pacote Salada",
+                Preco = 0,
+                Estoque = 500
+        };
+            var resultado = produto.Validacao();
+
+            Assert.False(resultado);
+
+        }
+
+        [Fact]
+        public void Estoque_Invalido_se_Valor_MenorqueZero()
+        {
+            var produto = new Produto
+            {
+                Nome = "Muçarela",
+                Preco = 45,
+                Estoque = -1
+            };
+
+            var resultado = produto.Validacao();
+
+            Assert.False(resultado);
+        }
+        [Fact]
+
+        public void Verificar_Nome_Nulo_ou_Vazio_Invalido()
+        {
+            var produto = new Produto
+            {
+                Nome = "",
+                Preco = 50,
+                Estoque = 45
+            };
+            var resultado = produto.Validacao();
+
+            Assert.False(resultado);
+        }
+        [Fact]
+        public void Verifica_PrecoMaiorqueZero_Estoque_MaiorqueZero_NomeProdutoExistente()
+        {
+            var produto = new Produto
+            {
+                Nome = "Proteína Isolada",
+                Preco = 115,
+                Estoque = 50
+            };
+
+            var resultado = produto.Validacao();
+
+            Assert.True(resultado);
+        }
+    }
+}
